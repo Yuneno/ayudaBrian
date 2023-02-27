17a84df96f0e1b715ae59a56af685e25b607cdac
Brian Pelegrin <brianjpdev@gmail.com>
2023-02-26 23:02:32 -0400

    Arreglando el problema de Api

---
+++ ArchivosTxt/ArchivosTxt/Controllers/GuardarTxTController.cs
@@ -0,0 +1,18 @@
+﻿using ArchivosTxt.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArchivosTxt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GuardarTxTController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult GuardarRegistros(List<Registro> registros)
+        {
+            return Ok(registros);
+        }
+    }
+}
--- ArchivosTxt/ArchivosTxt/Program.cs
+++ ArchivosTxt/ArchivosTxt/Program.cs
@@ -1,8 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,7 +20,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-
+app.MapControllers();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
